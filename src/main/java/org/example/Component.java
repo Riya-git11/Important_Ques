ef516dad5ac44936dd47d2b08c865cafeffe8743
Riya-git11 <riyagupta1047@gmail.com>
2025-07-20 00:30:12 +0530

    Initial commit

---
+++ src/main/java/org/example/Component.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Component {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
